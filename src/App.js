e8403a4bc2c818bf7382de8c3904879251252535
studyChan <midifei1d2r_@naver.com>
2021-05-12 12:36:29 +0900

    05/12 TodoList 디자인, 기능 연결

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {TextField, Typography} from '@material-ui/core';
+import {KeyboardDatePicker,KeyboardTimePicker} from '@material-ui/pickers'
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title : "",
+      content: "",
+      startDate: null,
+      startTime: null
+    }
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">TODO LIST</div>
+      <div className="input_area">
+        <TextField label="제목" size="normal" margin="normal" fullWidth required/>
+        <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline/>
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="yyyy/MM/DD"
+          margin="normal"
+          label="시작 예정일"
+          onChange={(value)=>console.log(value)}
+          style = {{width: '50%'}}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}/>
+
+        <KeyboardTimePicker
+          margin="normal"
+          label="시작시간"
+          variant="inline"
+          onChange={(value)=>console.log(value)}
+          style = {{width: '50%'}}
+          KeyboardButtonProps={{
+            'aria-label': 'change time',
+          }}/>
+      </div>
+      <div className="list_area">LIST AREA</div>
+      <Typography variant="body2" color="textSecondary" align="center">
+        { 'Copyright '+new Date().getFullYear()+'.' }
+      </Typography>
     </div>
   );
 }
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
+import "moment/locale/ko"
 
 ReactDOM.render(
+  <MuiPickersUtilsProvider utils={MomentUtils}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
+  </React.StrictMode>
+  </MuiPickersUtilsProvider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  );
